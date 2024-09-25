69546e06e21177b18d218dabaf5bd263fbc34285
Karine Brandelli Padilha <karine.padilha@ngi.com.br>
2024-09-25 15:53:01 -0300

    feat: create commonStyles file

---
+++ src/commonStyles.ts
@@ -0,0 +1,22 @@
+import { default as styled } from 'styled-components'
+
+export const Content = styled.div``
+
+export const Flex = styled.div`
+    display: flex;
+`
+
+export const FlexBetween = styled(Flex)`
+    align-items: center;
+    justify-content: space-between;
+`
+
+export const Wrapper = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    min-height: 100vh;
+    padding: 3rem;
+    box-sizing: border-box;
+`
